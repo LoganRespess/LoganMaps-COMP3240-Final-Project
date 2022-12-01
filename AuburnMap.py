6414f1898a9d8df459dac6e2916ba164e1768b96
Logan Respess <Logan.respess@gmail.com>
2022-11-30 23:59:49 -0600

    Very much close to finished. Ironing out the final kinks.

--- AuburnMap.py
+++ AuburnMap.py
@@ -4,36 +4,38 @@ import matplotlib.pyplot as plt
 
 auburnMap = nx.Graph()
 """adding our Nodes to the graph"""
-auburnMap.add_node("GinnConcourse")
-auburnMap.add_node("EngDr")
+
+
 auburnMap.add_node("Ross")
 auburnMap.add_node("haleyAtThatch")
-auburnMap.add_node("haleyAtMell")
+auburnMap.add_node("Haley At Mell")
 auburnMap.add_node("Mell")
-auburnMap.add_node("Quad")
-auburnMap.add_node("StudentCenter")
-auburnMap.add_node("MellAtQuad")
-auburnMap.add_node("Parker")
+auburnMap.add_node("The Quad")
+auburnMap.add_node("Student Center")
+auburnMap.add_node("Mell At The Quad")
 
+auburnMap.add_node("Parker")
+auburnMap.add_node("EngDr")
+auburnMap.add_node("GinnConcourse")
 """Connecting the Nodes with data from google maps. The weight is the distance between the nodes in meters."""
 nx.add_path(auburnMap,["GinnConcourse","EngDr"],weight=230)
 auburnMap.add_edge("GinnConcourse", "haleyAtThatch", weight = 250)
 auburnMap.add_edge("EngDr", "Ross", weight = 145)
-auburnMap.add_edge("Ross", "haleyAtMell", weight = 126)
-auburnMap.add_edge("haleyAtThatch", "haleyAtMell", weight = 215)
-auburnMap.add_edge("haleyAtMell", "Mell", weight = 111)
-auburnMap.add_edge("Mell", "MellAtQuad", weight = 100)
-auburnMap.add_edge("haleyAtThatch", "StudentCenter", weight = 200)
-auburnMap.add_edge("StudentCenter", "Parker", weight = 150)
-auburnMap.add_edge("MellAtQuad", "StudentCenter", weight = 209)
-auburnMap.add_edge("haleyAtThatch", "Quad", weight = 207)
-auburnMap.add_edge("haleyAtMell", "Quad", weight = 178)
-auburnMap.add_edge("Mell", "Quad", weight = 105)
-auburnMap.add_edge("MellAtQuad", "Quad", weight = 163)
-auburnMap.add_edge("StudentCenter", "Quad", weight = 200)
+auburnMap.add_edge("Ross", "Haley At Mell", weight = 126)
+auburnMap.add_edge("haleyAtThatch", "Haley At Mell", weight = 215)
+auburnMap.add_edge("Haley At Mell", "Mell", weight = 111)
+auburnMap.add_edge("Mell", "Mell At The Quad", weight = 100)
+auburnMap.add_edge("haleyAtThatch", "Student Center", weight = 200)
+auburnMap.add_edge("Student Center", "Parker", weight = 150)
+auburnMap.add_edge("Mell At The Quad", "Student Center", weight = 209)
+auburnMap.add_edge("haleyAtThatch", "The Quad", weight = 207)
+auburnMap.add_edge("Haley At Mell", "The Quad", weight = 178)
+auburnMap.add_edge("Mell", "The Quad", weight = 105)
+auburnMap.add_edge("Mell At The Quad", "The Quad", weight = 163)
+auburnMap.add_edge("Student Center", "The Quad", weight = 200)
 
 global nodeList 
-nodeList = ["GinnConcourse", "EngDr", "Ross", "haleyAtThatch", "haleyAtMell", "Mell", "Quad", "StudentCenter", "MellAtQuad", "Parker"]
+nodeList = ["GinnConcourse", "EngDr", "Ross", "haleyAtThatch", "Haley At Mell", "Mell", "The Quad", "Student Center", "Mell At The Quad", "Parker"]
 
 
 
@@ -68,10 +70,13 @@ def result(source, destination):
     pathLength = calcPathLength(path)
     #1.2 m/s is average walking speed
     estTime = pathLength / 1.2 / 60
-    print("The shortest distance between " + source + " and " + destination + " is " + pathString + ".")
-    print("The current length between the two locations is " + str(pathLength) + "m.")
-    print("Your estimated travel time is " + str(estTime) + " Minutes.")
-result("Quad", "Parker")
+    estTime = round(estTime, 2)
+    pathStr = "The shortest distance between " + source + " and " + destination + " is " + pathString + "."
+    lengthstr = "The current length between the two locations is " + str(pathLength) + "m."
+    timestr = "Your estimated travel time is " + str(estTime) + " Minutes."
+    strList = [pathStr, lengthstr, timestr]
+    return strList
+#result("The Quad", "Parker")
 #print(calcPathLength(quadToParker))
 #nx.draw(auburnMap, with_labels = True)
 #plt.show()
